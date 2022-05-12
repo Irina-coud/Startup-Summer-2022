ae1b9a6ed7b23db4a4582b053fffb6058e5edf19
Irina-coud <irinatobolich313@gmail.com>
2022-05-12 21:40:45 +0300

    feat: add UserNotRepositoriesPage

---
+++ src/components/UserNotRepositoriesPage/UserNotRepositoriesPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {
+  EmptyIcon,
+  InfoBox,
+  RepositoriesBox,
+  TextMessage,
+  UserBox,
+  UserDetails,
+  UserFollowers,
+  UserFollowing,
+  UserName,
+  UserNikname,
+  UserPhoto,
+  UserPhotoBox,
+  Wrapper,
+} from './UserNotRepositoriesPage.styled';
+
+export function UserNotRepositories() {
+  return (
+    <React.Fragment>
+      <Wrapper>
+        <UserBox>
+          <UserPhotoBox>
+            <UserPhoto />
+          </UserPhotoBox>
+          <UserDetails>
+            <UserName>Dan Abramov</UserName>
+            <UserNikname>gaearon</UserNikname>
+          </UserDetails>
+          <InfoBox>
+            <UserFollowers>65.8k followers</UserFollowers>
+            <UserFollowing>171 following</UserFollowing>
+          </InfoBox>
+        </UserBox>
+        <RepositoriesBox>
+          <EmptyIcon />
+          <TextMessage>Repository list is empty</TextMessage>
+        </RepositoriesBox>
+      </Wrapper>
+    </React.Fragment>
+  );
+}
